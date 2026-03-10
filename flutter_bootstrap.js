9a4d99694222957b2310b51251a8781a125c3329
crazygo <crazygo@users.noreply.github.com>
2026-03-10 00:51:24 +0000

    deploy: a0cf278e05e10ef9b994c24fbc234fcb6ad342d0

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "773517088" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1728579579" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
